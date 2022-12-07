f2e6f21067af0e4494d0d00b9108714ad428c7a1
Lucas Martino <lucasmartino402@gmail.com>
2022-12-06 23:49:41 -0300

    Problema de calculadora solucionado

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,25 +1,25 @@
-let inputNombreProducto = document.querySelector("#inputNombreProducto").value,
-inputPrecioLista = document.querySelector("#inputPrecioLista").value,
-inputCostosVariables = document.querySelector("#inputCostosVariables").value,
-inputCostosFijos = document.querySelector("#inputCostosFijos").value,
-inputMargen = document.querySelector("#inputMargen").value,
-btnCalcular = document.querySelector("#btnCalcular"),
-h4Calculadora = document.querySelector("#h4Calculadora");
+const inputNombreProducto = document.querySelector("#inputNombreProducto");
+const inputPrecioLista = document.querySelector("#inputPrecioLista");
+const inputCostosVariables = document.querySelector("#inputCostosVariables");
+const inputCostosFijos = document.querySelector("#inputCostosFijos");
+const inputMargen = document.querySelector("#inputMargen");
+const btnCalcular = document.querySelector("#btnCalcular");
+const h4Calculadora = document.querySelector("#h4Calculadora");
 
 // FUNCIONES
 class CalcularPrecio{
 
     constructor(nombreProducto, precioDeLista, costosVariables, costosFijos, margenDeGanancia){
         this.nombreProducto = nombreProducto;
-        this.precioDeLista = parseInt(precioDeLista);
-        this.costosVariables = parseInt(costosVariables);
-        this.costosFijos = parseInt(costosFijos);
-        this.margenDeGanancia = parseFloat(margenDeGanancia);
+        this.precioDeLista = precioDeLista;
+        this.costosVariables = costosVariables;
+        this.costosFijos = costosFijos;
+        this.margenDeGanancia = margenDeGanancia;
         this.precio = 0;
     };
 
     calcularPrecio(){
-       return this.precio = parseInt((this.costosFijos + this.costosVariables + this.precioDeLista) * this.margenDeGanancia);
+       this.precio = parseInt((this.costosFijos + this.costosVariables + this.precioDeLista) * this.margenDeGanancia);
     }
 };
 function precioProducto(obj){
@@ -42,9 +42,18 @@ function limpiarCampos(){
 btnCalcular.addEventListener("click", (e)=>{
     e.preventDefault();
 
-    const precioProduct1 = new CalcularPrecio(inputNombreProducto, inputPrecioLista, inputCostosVariables, inputCostosFijos, inputMargen);
+    const namePr = inputNombreProducto.value;
+    const prLista = parseInt(inputPrecioLista.value);
+    const ctVar = Number(inputCostosVariables.value);
+    const ctFij = Number(inputCostosFijos.value);
+    const marg = Number(inputMargen.value);
+
+    const precioProduct1 = new CalcularPrecio(namePr, prLista, ctVar, ctFij, marg);
     precioProduct1.calcularPrecio();
-    console.log(inputNombreProducto, inputPrecioLista, inputCostosVariables, inputCostosFijos, inputMargen)
+
+    console.log(precioProduct1);
+    console.log(inputNombreProducto, prLista, ctVar, ctFij, marg);
+
     HTMLPrecio(precioProduct1);
     // limpiarCampos();
 })
